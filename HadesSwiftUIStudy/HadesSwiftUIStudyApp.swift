5e8cbcd9a3ce13d08cd7774190b75133df31c0e8
Esdras Santos <esdrassantos@MacBook-Pro-de-Esdras.local>
2024-09-05 22:10:39 -0300

    initial

---
+++ HadesSwiftUIStudy/HadesSwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HadesSwiftUIStudyApp.swift
+//  HadesSwiftUIStudy
+//
+//  Created by Esdras Santos on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HadesSwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
