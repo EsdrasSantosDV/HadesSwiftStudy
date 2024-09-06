1042ea7ad83caed0f2c9d5a17fe371b4e13ea0e1
Esdras Santos <esdrassantos@MacBook-Pro-de-Esdras.local>
2024-09-05 22:33:50 -0300

    imagem como colocar

--- HadesSwiftUIStudy/ContentView.swift
+++ HadesSwiftUIStudy/ContentView.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+           VStack(alignment: .leading) {
+               Text("Turtle Rock")
+                   .font(.title)
+               HStack {
+                   Text("Joshua Tree National Park")
+                       .font(.subheadline)
+                   Spacer()
+                   Text("California")
+                       .font(.subheadline)
+               }
+           }
+           .padding()
+       }
 }
 
 #Preview {
---
+++ ImagemTutorial.swift
@@ -0,0 +1,23 @@
+//
+//  ImagemTutorial.swift
+//  HadesSwiftUIStudy
+//
+//  Created by Esdras Santos on 05/09/24.
+//
+
+import SwiftUI
+
+struct ImagemTutorial: View {
+    var body: some View {
+        //COMO COLOCAR IMAGEM
+        Image("goku").clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    ImagemTutorial()
+}
